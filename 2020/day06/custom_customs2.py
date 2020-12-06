cb04729ad0e83d1182c63d94ad776856232226e1
lapssh <lapssh@yandex.ru>
2020-12-06 15:58:23 +0300

    День 6-й сделана вторая это победа

--- 2020/day06/custom_customs2.py
+++ 2020/day06/custom_customs2.py
@@ -5,39 +5,36 @@ def get_input(filename):
 
 
 def prepare_data(data):
-    # print(f'{data=}')
     data2 = data.split('\n\n')
-    # print(f'{data2=}')
     data3 = []
     for i in data2:
-        i_ = i.replace('\n', ' ')
-        j = i_.split(' ')
-        # print(f'{i=} {i_=} {j=}')
+        j = i.strip().split('\n')
+
         data3.append(j)
-    # print(f'{data3=}')
+
+
+    print(len(data3), 'длинна')
+    print(data3)
     return data3
 
 
 def get_count(data):
     # identify the questions to which everyone answered "yes"
-    count_ = 0
+    answer_count = 0
     num_of_groups = len(data)
     print(f'{num_of_groups=}')
-    for item in data:
-        person_count = len(item)
-        # print(f'В группе {item=} {person_count=} человек')
-        result = set(item[0])
-        # print(result)
-        count2 = 0
-        for i in item:
-            tmp_set = set(i)
-            set_diff = result.intersection(tmp_set)
-            # print(item, ' ', end='')
-        count_ += len(set_diff)
-        count2 = len(set_diff)
-        print('\nВ группе ',item, count2, 'совпадений')
+    for group in data:
+        result = set(group[0])
+        person_count = len(group)
+        print(f'В группе {group=} {person_count=} человек')
 
-    print(f'{count_=}')
+
+        for answer in range(1, person_count):
+            result.intersection_update(group[answer])
+            if len(result) == 0:
+                break
+        answer_count += len(result)
+    print(f'{answer_count=}')
 
 def test01():
     # must return 6
@@ -70,10 +67,12 @@ tv
 '''
     return data
 #
-data = get_input('input.txt')
+data = get_input('input2.txt')
 # data = test01()
 # data = test02()
 data = prepare_data(data)
 get_count(data)
 # print(test01())
 # 3447 - wrong
+# 3375 - wrong
+# 3382 - right
