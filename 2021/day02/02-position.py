7e7e176c47de7ca837153e0f50eb380e888ca676
Aleksey Lapshin <lapssh@yandex.ru>
2021-12-02 09:50:20 +0300

    day02 task 2 - complite

--- 2021/day02/02-position.py
+++ 2021/day02/02-position.py
@@ -22,9 +22,30 @@ def calculate_position(data):
     return x * y
 
 
-filename = 'test_input.txt'
-print(load_data(filename))
-print('new coordinates multiply:', calculate_position(load_data(filename)))
+def calculate_with_aim(data):
+    x, depth = 0, 0
+    aim = 0
+    for i in data:
+        if i[0] == 'forward':
+            x = x + i[1]
+            depth = depth + (i[1] * aim)
+        elif i[0] == 'down':
+            aim = aim + i[1]
+        elif i[0] == 'up':
+            aim = aim - i[1]
+        else:
+            print('error data!')
+    print('position with aim is ', x, depth)
+    return x * depth
 
+
+test_file = 'test_input.txt'
 filename = 'input.txt'
+print(load_data(test_file))
+print('\n', 'Task #1')
+print('new coordinates multiply:', calculate_position(load_data(test_file)))
+print()
 print('new coordinates multiply:', calculate_position(load_data(filename)))
+print('\n', 'Task #2')
+print('coordinates with aim test:', calculate_with_aim(load_data(test_file)))
+print('coordinates with aim:', calculate_with_aim(load_data(filename)))
