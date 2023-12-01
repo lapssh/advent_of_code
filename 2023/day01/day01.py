774b5b6367ae4a33fe93d8ea2f655a126290bd57
Лапшин Алексей Владимирович <lapshin_av@corp.atomsro.ru>
2023-12-01 16:27:57 +0300

    day01 task2 complite

--- 2023/day01/day01.py
+++ 2023/day01/day01.py
@@ -1,6 +1,16 @@
 import re
 
-
+nums = {
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9',
+}
 def load(file='input.txt'):
     with open(file, "rt") as f:
         return list(line.strip() for line in f.readlines())
@@ -21,9 +31,7 @@ def calibrate_line(line):
     return int(line)
 
 
-def search_nums(part, nums):
-    start = 0
-    end = len(part)
+def search_nums(part):
     word = ''
     while len(part) > 1:
         for letter in part:
@@ -39,9 +47,7 @@ def search_nums(part, nums):
         # print(f'отрезаем {part=}')
 
 
-def search_nums2(part, nums):
-    start = 0
-    end = len(part)
+def search_nums2(part):
     word = ''
     part = str(part[::-1])
     # print(f'{part=}')
@@ -59,54 +65,36 @@ def search_nums2(part, nums):
         # print(f'отрезаем {part=}')
 
 
-def parse_block(block, nums):
+def parse_block(block):
     # print('===========ИЩЕМ СЛЕВА============')
     # print(f'{block=}')
-    words_for_replace = []
     while True:
         if block[0].isdigit():
             return block
-        word = search_nums(block, nums)
+        word = search_nums(block)
         # print(f'{block=} {word=}')
         if not word: break
-        print(f'в {block=} найдено {word=}')
+        # print(f'в {block=} найдено {word=}')
         block = str(block).replace(word, nums[word])
         return block
     return block
-    #     block = str(block).replace(word, nums[word])
-    #     print(f'Теперь {block=}')
-    #     return block
-    # return block
 
-
-def parse_block2(block, nums):
+def parse_block2(block):
     # print('===========ИЩЕМ СПРАВА============')
     # print(f'{block=}')
-    words_for_replace = []
     while True:
         if block[-1].isdigit():
             return block
-        word = search_nums2(block, nums)
+        word = search_nums2(block)
         # print(f'{block=} {word=}')
         if not word: break
-        print(f'в {block=} найдено {word=}')
+        # print(f'в {block=} найдено {word=}')
         block = str(block).replace(word, nums[word])
         return block
     return block
 
 
 def calibrate_line_advanced(line):
-    nums = {
-        'one': '1',
-        'two': '2',
-        'three': '3',
-        'four': '4',
-        'five': '5',
-        'six': '6',
-        'seven': '7',
-        'eight': '8',
-        'nine': '9',
-    }
     # print(f'src-line:', line)
     # разбиваем строку на блоки текста
     res = re.findall(r'\D+', line)
@@ -115,20 +103,13 @@ def calibrate_line_advanced(line):
         # парсим каждый блок на цифры
         for i in res:
             # проход слева
-            block = parse_block(line, nums)
-            print(f'После прохода слева {block=}')
+            left = parse_block(line)
+            # print(f'После прохода слева {left=}')
             # проход справа
-            block = parse_block2(block, nums)
-            print(f'После прохода справа {block=}')
-            # if left:
-            #     line.replace(left, nums[left])
-            # right = parse_block2(line, nums)
-            # if right:
-            #     line.replace(right, nums[left])
-            # line = parse_block(line, nums)
-            # line = parse_block2(line, nums)
-            # print(f'{line=}')
-            return calibrate_line(block)
+            right = parse_block2(line)
+            # print(f'После прохода справа {right=}')
+            # print(left+right)
+            return calibrate_line(left+right)
     else:
         return calibrate_line(line)
     exit()
@@ -146,7 +127,7 @@ def part2(data):
     acc = 0
     for line in data:
         n = calibrate_line_advanced(line)
-        print(f'{n=}')
+        # print(f'{n=}')
         if 9 > n > 99:
             print('ERROR')
             exit()
@@ -157,11 +138,11 @@ def part2(data):
 data = load()
 # data = data[0:5]
 # data = ['1abc2',     'pqr3stu8vwx',     'a1b2c3d4e5f',     'treb7uchet' ]
-data = ['twone',     'pqr3stu8vwx',     'a1b2c3d4e5f',     'treb7uchet' ]
+# data = ['twone',     'pqr3stu8vwx',     'a1b2c3d4e5f',     'treb7uchet' ]
 # data = ['1abc2',     'pqr3stu8vwx',     'a1b2c3d4e5f',     'treb7uchet' ]
 # data = ['two1nine',  'abcone2threexyz', '9one1two']
 # data = ['eightwothree', 'two1nine',  'abcone2threexyz', 'xtwone3four', '4nineeightseven2', 'zoneight234', '7pqrstsixteen']
 # data = ['twoneighthree', 'eightwothree']
 # print(f'test: ', calibrate_line(data[0]))
-# print(f'part1: ', part1(data))
+print(f'part1: ', part1(data))
 print(f'part2: ', part2(data))  # 54594
