f658e9ca503e60fc45ecfc2c0cd29049602fbfda
Aleksey Lapshin <lapssh@yandex.ru>
2023-12-07 22:57:50 +0300

    почти готово, ошибка в определении пара - 2 пары

--- 2023/day07/day07.py
+++ 2023/day07/day07.py
@@ -3,6 +3,31 @@ def load(file='input.txt'):
         return list(line.strip() for line in f.readlines())
 
 ranked_cards = []
+type_ranks = {
+    'High': 1,
+    'Pare': 2,
+    'TwoPare': 3,
+    'Three': 4,
+    'FullHouse': 5,
+    'Care': 6,
+    'Five': 7,
+}
+
+type_cards = {
+    'A': 13,
+    'K': 12,
+    'Q': 11,
+    'J': 10,
+    'T': 9,
+    '9': 8,
+    '8': 7,
+    '7': 6,
+    '6': 5,
+    '5': 4,
+    '4': 3,
+    '3': 2,
+    '2': 1,
+}
 
 def get_card_type(card):
     # print(card)
@@ -37,13 +62,22 @@ def get_card_type(card):
         elif len(set(card)) == 3:
             return 'TwoPare'
         else:
-            print('Two', card)
-            return 'Two'
+            # print('Pare', card)
+            return 'Pare'
     else:
-        print('Two', card)
-        return 'Two'
+        # print('Pare', card)
+        return 'Pare'
     # проверяем пару
 
+def get_stronger(card, _card):
+    print(f'Равные карты {card=} {_card=}')
+    for symbol in range(len(card)):
+        if type_cards[card[symbol]] > type_cards[_card[symbol]]:
+            print(f' {card=} > {_card=} {card[symbol]} > {_card[symbol]}')
+            return '>'
+        elif type_cards[card[symbol]] < type_cards[_card[symbol]]:
+            print(f' {card=} > {_card=} {card[symbol]} < {_card[symbol]}')
+            return '<'
 
 
 def card_is_higher(card, _card):
@@ -52,27 +86,64 @@ def card_is_higher(card, _card):
     if type_card1 == None:
         print(card, type_card1, 'Ошибка')
         exit()
-    print(type_card1, card)
+    # print(type_card1, card)
     type_card2 = get_card_type(_card)
+    # проверяем чья раздача лучше
+    if type_ranks[type_card1] == type_ranks[type_card2]:
+        # Необходимо определить старшую букву
+        return(get_stronger(card, _card))
+        # print(f'{type_ranks[type_card1]} = {type_ranks[type_card2]}')
+        return '>'
+    if type_ranks[type_card1] > type_ranks[type_card2]:
+        # print(f'{type_ranks[type_card1]} > {type_ranks[type_card2]}')
+        return '>'
+    elif type_ranks[type_card1] < type_ranks[type_card2]:
+        # print(f'{type_ranks[type_card1]} < {type_ranks[type_card2]}')
+        return '<'
+    else:
+        print('Карты равны')
+        exit()
+        return None
 def insert_card(card):
     # если карта первая - просто добавляем
     if ranked_cards == []:
         ranked_cards.append(card.split())
     else:
-        for _card in ranked_cards:
-            if card_is_higher(card.split()[0], _card[0]):
-                pass
-               # print(f'{card=} Больше, чем {_card=}')
+        idx = 0
+        while True:
+            if idx == len(ranked_cards):
+                print(f'КОНЕЦ Шаг {idx} {card=} ')
+                ranked_cards.append(card.split())
+                break
+            card2 = ranked_cards[idx]
+            ans = card_is_higher(card.split()[0], card2[0])
+            if ans == '<':
+                # ranked_cards.insert(idx, card.split())
+                print(f'Шаг {idx} {card=} {ans=} {card2=}')
+                ranked_cards.insert(idx, card.split())
+                break
+            elif ans == '>':
+                print(f'Шаг {idx} {card=} {ans=} {card2=}')
+                idx += 1
 
+def calc_bid():
+    total  = 0
+    for mult, card in enumerate(ranked_cards):
+        bid = int(card[1])
+        total += (mult + 1) * bid
+        print(f'{bid=}  {mult+1} {total=}')
+    return total
 
 
 def part1(data):
     for card in data:
         insert_card(card)
     print(ranked_cards)
+    return calc_bid()
 
-# data = load()
-data = load('test.txt')
+
+data = load()
+# data = load('test.txt')
 print(data)
 
-print(part1(data))
+print('Part1 result:', part1(data))
