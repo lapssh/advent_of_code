24884056f08f92bd6d3ccaad0956422afd0494e3
Aleksey Lapshin <lapssh@yandex.ru>
2024-11-28 09:48:01 +0300

    day03 - 2019 in progress

--- 2019/day03.py
+++ 2019/day03.py
@@ -17,57 +17,59 @@ def part1(src_data):
         match i[0]:
             case 'R':
                 target = int(i[1:])
+                [coords1.append((x, y1)) for x in range(x1, target + 1)]
                 x1 += target
             case 'L':
                 target = int(i[1:])
+                [coords1.append((x, y1)) for x in range(x1 - 1, x1 - target - 1, -1)]
                 x1 -= target
             case 'U':
                 target = int(i[1:])
+                [coords1.append((x1, y)) for y in range(y1, target + 1)]
                 y1 += target
             case 'D':
                 target = int(i[1:])
+                [coords1.append((x1, y)) for y in range(y1, y1 - target - 1, -1)]
                 y1 -= target
-        coords1.append((x1, y1))
-    print(f'{x1=}, {y1=}')
     for i in src_data[1]:
         match i[0]:
             case 'R':
-                x2 += int(i[1:])
+                target = int(i[1:])
+                [coords2.append((x, y2)) for x in range(x2, target + 1)]
+                x2 += target
             case 'L':
-                x2 -= int(i[1:])
+                target = int(i[1:])
+                print(f'LEFT {target=}')
+                print(f'{list(range(x2-1, x2-target-1, -1))=}')
+                [coords2.append((x, y2)) for x in range(x2 - 1, x2 - target - 1, -1)]
+                x2 -= target
             case 'U':
-                y2 += int(i[1:])
+                target = int(i[1:])
+                [coords2.append((x2, y)) for y in range(y2, target + 1)]
+                y2 += target
             case 'D':
-                y2 -= int(i[1:])
-        coords2.append((x2, y2))
+                target = int(i[1:])
+                [coords2.append((x2, y)) for y in range(y2, y2 - target - 1, -1)]
+                y2 -= target
+
     print(f'{coords1=}, {coords2=}')
-    pull1 = build_pull(coords1)
-    print(f'{pull1=}')
-    result = src_data
+    coords1 = set(coords1)
+    coords2 = set(coords2)
+    print(f'{coords1=}, {coords2=}')
+    result = coords1.intersection(coords2)
+    print(f'{result=}')
+    result = sorted(list(result))
+    result.remove((0, 0))
+    print(f'{result=}')
+    # ищем манхэтенское расстояние
+    min_dist = 100000000
+    for point in result:
+        manheten_dist = abs(point[0] - 0) + abs(point[1] - 0)
+        if manheten_dist < min_dist:
+            min_dist = manheten_dist
+    print(f'{min_dist=}')
+
     return result
-
-
-def build_pull(coords: list) -> set:
-    start = coords[0]
-    coords = coords[1:]
-    for i in coords:
-        x1, y1 = start
-        print(f'{x1=}, {y1=}')
-        x2, y2 = i[0], i[1]
-        if x2 > x1:
-            for i in range(x1 + 1, x2 + 1):
-                coords.append((i, y2))
-        else:
-            for i in range(x2 - 1, x1, -1):
-                coords.append((i, y2))
-        if y2 > y1:
-            for i in range(y1 + 1, y2 + 1):
-                coords.append((x2, i))
-        else:
-            for i in range(y2 - 1, y1, -1):
-                coords.append((x2, i))
-        start = (x2, y2)
-    return set(coords)
 
 
 def part2(src_data):
@@ -82,7 +84,8 @@ def show_results(answer1, answer2):
 
 if __name__ == '__main__':
     # data = load()
-    data = load('test-day03-1.txt')
+    # data = load('test-day03-1.txt')
+    data = load('test-day03-2.txt')
     part1_result = part1(data)
     part2_result = part2(data)
     show_results(part1_result, part2_result)
