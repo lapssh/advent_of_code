f53c9d153d33b0185288952dd3815472b796ba3b
Aleksey Lapshin <lapssh@yandex.ru>
2024-12-09 21:20:59 +0300

    day 09 in progress

--- 2024/day09.py
+++ 2024/day09.py
@@ -1,26 +1,47 @@
 def load(file='day_test.txt'):
     with open(file, "rt") as f:
         lines = list(x.replace('\n', '') for x in f.readlines())
-        print(lines)
+        # print(lines)
         data = []
         for item in lines:
             _ = list(item)
-            print(_)
+            # print(f'{_=}')
+        _ = [int(x) for x in _]
         data.append(_)
-
         # res = [x.split(',') for x in lines]
-        print(f'{data=}')
-        for line in data:
-            print(line)
+
         return data
 
 
 def part1(src_data):
-    return result
+    def is_file(idx):
+        if idx % 2 == 0:
+            return False
+        else:
+            return True
+
+    data = src_data[0]
+    print(data)
+    fs_map = []
+    file_id = 0
+    for idx,i in enumerate(data):
+        print(i, is_file(idx))
+        if is_file(idx):
+            for j in range(i):
+                fs_map.append(file_id)
+            file_id += 1
+        else:
+            for j in range(i):
+                fs_map.append('.')
+
+
+    print(f'{fs_map=}')
+
+    return data
 
 
 def part2(src_data):
-    return result
+    return src_data
 
 
 def show_results(answer1, answer2):
@@ -29,7 +50,8 @@ def show_results(answer1, answer2):
 
 
 if __name__ == '__main__':
-    data = load('day09-test1.txt')
+    # data = load('day09-test1.txt')
+    data = load('day09-test2.txt')
     # data = load('day09-input.txt')
     part1_result = part1(data)
     part2_result = part2(data)
