78b35db00a81ef3470096c0ac7367d5d939458db
Aleksey Lapshin <lapssh@yandex.ru>
2024-12-12 20:55:35 +0300

    day 09 complite

--- 2024/day09.py
+++ 2024/day09.py
@@ -1,3 +1,6 @@
+from traceback import print_tb
+
+
 def load(file='day_test.txt'):
     with open(file, "rt") as f:
         lines = list(x.replace('\n', '') for x in f.readlines())
@@ -16,16 +19,16 @@ def load(file='day_test.txt'):
 def part1(src_data):
     def is_file(idx):
         if idx % 2 == 0:
-            return False
-        else:
             return True
+        else:
+            return False
 
     data = src_data[0]
     print(data)
     fs_map = []
     file_id = 0
     for idx,i in enumerate(data):
-        print(i, is_file(idx))
+        # print(idx, is_file(idx))
         if is_file(idx):
             for j in range(i):
                 fs_map.append(file_id)
@@ -35,9 +38,35 @@ def part1(src_data):
                 fs_map.append('.')
 
 
-    print(f'{fs_map=}')
+    # print(f'{fs_map=}')
+    x1 = 0
+    x2 = len(fs_map)-1
+    hvost = []
+    len_map = len(fs_map)
+    while '.' in fs_map:
+        # print(f'{x1=} {x2=} {fs_map=}')
+        # print(f'{fs_map[x1]=} ')
+        print(fs_map[x1])
+        if fs_map[x1] == '.':
+            # print('первая проверка')
+            # hvost.append('.')
+            if fs_map[-1] == '.':
+                hvost.append(fs_map.pop())
+                continue
+            else:
+                fs_map[x1] = fs_map.pop()
+        else:
+            x1 += 1
+    # print('end')
+    # print(fs_map)
+    # print(f'{len_map=}')
 
-    return data
+    # считаем контрольную сумму
+    ctrl_sum = 0
+    for idx, num in enumerate(fs_map):
+        ctrl_sum += idx * num
+    print(ctrl_sum)
+    return ctrl_sum
 
 
 def part2(src_data):
@@ -46,13 +75,13 @@ def part2(src_data):
 
 def show_results(answer1, answer2):
     print(f'Result part1: {answer1}')
-    print(f'Result part2: {answer2}')
+    # print(f'Result part2: {answer2}')
 
 
 if __name__ == '__main__':
     # data = load('day09-test1.txt')
-    data = load('day09-test2.txt')
-    # data = load('day09-input.txt')
+    # data = load('day09-test2.txt')
+    data = load('day09-input.txt')
     part1_result = part1(data)
     part2_result = part2(data)
     show_results(part1_result, part2_result)
